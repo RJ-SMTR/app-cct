7a3c575b4f41e80d313a575c33c5f206eac27320
Gabriel0109 <gabrielglfg@outlook.com>
2023-07-14 11:46:02 -0300

    feat: página de perfil

--- src/app/configs/routesConfig.js
+++ src/app/configs/routesConfig.js
@@ -9,6 +9,7 @@ import ResetPasswordConfig from '../main/pages/auth/password/reset/ResetPassword
 import ConcludeConfig from '../main/pages/auth/conclude/ConcludeConfig';
 import Error404Page from '../main/404/Error404Page';
 import ExampleConfig from '../main/example/ExampleConfig';
+import profileAppConfig from '../main/pages/profile/profileAppConfig';
 
 const routeConfigs = [
   ExampleConfig,
@@ -17,6 +18,7 @@ const routeConfigs = [
   ForgotPasswordConfig,
   ResetPasswordConfig,
   ConcludeConfig,
+  profileAppConfig
 ];
 
 const routes = [
