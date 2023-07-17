fd634bf057332d191474c7c6f60ae03706bfe11a
Perim <gsperim@gmail.com>
2023-07-17 10:28:32 -0300

    fix: update dependency versions
    add .npmrc to add especial configs to build
    update npm package versions
    add suport do ENV var to update URL_BASE api URI

--- src/app/configs/api/api.js
+++ src/app/configs/api/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
-let baseURL = '';
-if (window.location.hostname === 'localhost') {
-    baseURL = 'http://localhost:3001/api/v1/'
-}
 
+const baseURL = process.env.BASE_URL_CCT || 'http://localhost:3001/api/v1/'
 
 export const api = axios.create({ baseURL })
