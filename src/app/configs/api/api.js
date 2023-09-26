6b5d271225b60cd90f3bdda72cbd5dc9426c3635
Ruiz <alexander.rivail@gmail.com>
2023-09-26 12:22:51 -0300

    kub

--- src/app/configs/api/api.js
+++ src/app/configs/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = process.env.BASE_URL_CCT || 'http://localhost:3000/api/v1/'
+const baseURL = process.env.BASE_URL_CCT || 'https://api.cct.cluster.mobilidade.dev/api/v1/'
 
 export const api = axios.create({ baseURL })
