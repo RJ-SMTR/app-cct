de5166424645d453eac04f9acfef26b06bb2dd56
Ruiz <alexander.rivail@gmail.com>
2023-10-09 12:51:12 -0300

    fix env

--- src/app/configs/api/api.js
+++ src/app/configs/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = process.env.BASE_URL_CCT || 'https://api.cct.cluster.mobilidade.dev/api/v1/'
+const baseURL = process.env.BASE_URL_CCT || 'https://api.cct.hmg.mobilidade.rio/api/v1/'
 
 export const api = axios.create({ baseURL })
