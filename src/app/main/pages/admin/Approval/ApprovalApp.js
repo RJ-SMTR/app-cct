6f979e0d1c6d824b3ba71f5cb88d542802e320b6
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2024-03-27 16:48:17 -0300

    feat: fltro de data att

--- src/app/main/pages/admin/Approval/ApprovalApp.js
+++ src/app/main/pages/admin/Approval/ApprovalApp.js
@@ -2,9 +2,7 @@ import { Box, Paper } from "@mui/material";
 import CardBalance from "./components/CardBalance";
 import DataGridInfos from "./components/DataGrid";
 import CardSelection from "./components/CardSelection";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { handleAuthValue, setSelectedPeriod } from "app/store/releaseSlice";
+import { useSelector } from "react-redux";
 
 
 
@@ -21,7 +19,7 @@ function ApprovalApp() {
             <div className="p-24 pt-10">
             <Box className='flex flex-col md:flex-row mt-24 max-w-[684px] spacing-x-2'>
                 <CardBalance type={"Saldo da Banco"} amount={100000000}/>
-                    {selectedPeriod ? <CardBalance type={"Valor Autorizado"} amount={authValue} />: null}
+                    {authValue ? <CardBalance type={"Valor Autorizado"} amount={authValue} />: null}
             </Box>
                 <CardSelection/>
                 {selectedPeriod ? <Box>
