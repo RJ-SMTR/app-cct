9b0db83afec4660298a94cc856f1cd4026aa7437
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2024-04-25 13:11:48 -0300

    Update releaseSlice.js

--- src/app/store/releaseSlice.js
+++ src/app/store/releaseSlice.js
@@ -46,7 +46,6 @@ export default stepSlice.reducer;
 export const getData = (data) => (dispatch) => {
     const token = window.localStorage.getItem('jwt_access_token');
 
-    console.log(data)
 
     api.get(jwtServiceConfig.finanGetInfo + `?mes=${data.selectedDate.mes}&periodo=${data.selectedDate.periodo}&ano=${data.selectedYear}&autorizado=${data.selectedStatus?.status}`, {
         headers: { "Authorization": `Bearer ${token}` },
