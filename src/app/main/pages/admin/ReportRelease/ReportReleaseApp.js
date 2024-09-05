673bc3b39cc44464b551505581638087c3d58184
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2024-09-05 15:44:45 -0300

    feat: relatório para lançamentos

--- src/app/main/pages/admin/ReportRelease/ReportReleaseApp.js
+++ src/app/main/pages/admin/ReportRelease/ReportReleaseApp.js
@@ -1,7 +1,20 @@
+import { Box, Paper, Card, FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import DataGridInfos from './components/DataGrid';
+import { useDispatch } from "react-redux";
+
+
 
 function ReportReleaseApp() {
   return (
-    <div>ReportReleaseApp</div>
+    <>
+      <div className="p-24 pt-10">
+        <Typography className='font-medium text-3xl'>Relatório Lançamentos</Typography>
+        <Box>
+        <DataGridInfos />
+        </Box>
+      </div>
+    </>
   )
 }
 
