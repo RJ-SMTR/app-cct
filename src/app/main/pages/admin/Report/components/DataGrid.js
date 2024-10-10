dff6917a2968f6bbf77318d5d6440d961a3cbe5b
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2024-10-10 11:22:35 -0300

    fix: ordem data aviso

--- src/app/main/pages/admin/Report/components/DataGrid.js
+++ src/app/main/pages/admin/Report/components/DataGrid.js
@@ -605,7 +605,7 @@ export default function BasicEditingGrid() {
                             </Box>
                             {whichStatusShow.includes("A pagar") && (
                                 <span className="text-sm text-red-600">
-                                    Atenção: Para o status "a pagar", a data escolhida deve ser referente a data da transação (quarta a quinta-feira).
+                                    Atenção: Para o status "a pagar", a data escolhida deve ser referente a data da transação (quinta a quarta-feira).
                                 </span>
                             )}
                             <Box>
