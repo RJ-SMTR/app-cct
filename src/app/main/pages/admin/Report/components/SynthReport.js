cf80e0e8560eecbca3ee7f91124c2e9fe077cca4
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2024-10-10 11:10:15 -0300

    fix: Status de aguardando pagamento

--- src/app/main/pages/admin/Report/components/SynthReport.js
+++ src/app/main/pages/admin/Report/components/SynthReport.js
@@ -446,7 +446,7 @@ export default function BasicEditingGrid() {
             case 'naopago':
                 return 'Erro';
             default:
-                return '';
+                return status;
         }
     };
 
