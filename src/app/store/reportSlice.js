cf2cdbb4c8c1bf80a118b48d90ec20d13e91656e
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2024-10-29 17:24:01 -0300

    feat: relatório analítico novo layout

--- src/app/store/reportSlice.js
+++ src/app/store/reportSlice.js
@@ -145,7 +145,7 @@ export const handleReportInfo = (data, reportType) => async (dispatch) => {
                         valor: responseData.reduce((sum, curr) => sum + curr.valor, 0),
                         status: 'Todos'
                     };
-                    if(reportType == 'sintetico'){
+                    if(reportType == 'sintetico' || reportType == 'analitco'){
                         dispatch(handleSynthData(combinedResponse))
                     } else {
 
