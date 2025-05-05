438b5c589d9ecc9ba8fe27b8afe1b71aba1beced
MatthewAraujo <matthewaraujo20@gmail.com>
2025-05-05 11:14:11 -0300

    adicionando disbled nos relatorios

--- src/app/main/pages/admin/Report/ReportApp.js
+++ src/app/main/pages/admin/Report/ReportApp.js
@@ -30,30 +30,31 @@ function ReportApp() {
 		dispatch(setReportType(selectedReport));
 	}, [selectedReport]);
 
-    return (
-        <>
-            <div className="p-24 pt-10">
-                <Card className="w-full p-24 relative mt-32">
-                    <header className="flex justify-between items-center">
-                        <h3 className="font-semibold mb-24">
-                            Seleção de Relatórios
-                        </h3>
-                    </header>
-                    <FormControl style={{minWidth: '20rem'}}>
-                        <InputLabel id="report-select-label">Selecionar Relatório</InputLabel>
-                        <Select
-                            labelId="report-select-label"
-                            id="report-select"
-                            value={selectedReport}
-                            label="Selecionar Relatório"
-                            onChange={handleSelectChange}
-                        >
-                            <MenuItem value="analitico">Relatório Analítico</MenuItem>
-                            <MenuItem value="sintetico">Relatório Sintético</MenuItem>
-                            <MenuItem value="consolidado">Relatório Consolidado</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Card>
+	return (
+		<>
+			<div className="p-24 pt-10">
+				<Card className="w-full p-24 relative mt-32">
+					<header className="flex justify-between items-center">
+						<h3 className="font-semibold mb-24">Seleção de Relatórios</h3>
+					</header>
+					<FormControl style={{ minWidth: "20rem" }}>
+						<InputLabel id="report-select-label">
+							Selecionar Relatório
+						</InputLabel>
+						<Select
+							labelId="report-select-label"
+							id="report-select"
+							value={selectedReport}
+							label="Selecionar Relatório"
+							onChange={handleSelectChange}
+						>
+							<MenuItem value="analitico" className="Mui-disabled">Relatório Analítico</MenuItem>
+							<MenuItem value="consolidado">Relatório Consolidado</MenuItem>
+							<MenuItem value="sintetico" className="Mui-disabled">Relatório Sintético</MenuItem>
+							<MenuItem value="Movimentação Financeira">Relatório Movimentação Financeira</MenuItem>
+						</Select>
+					</FormControl>
+				</Card>
 
                 <Box>
                         {selectedReport === 'analitico' && <DataGridInfos />}
