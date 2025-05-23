31a5870fe46cb03592875035e714616604abfb9e
Gabriel0109 <82231197+Gabriel0109@users.noreply.github.com>
2025-05-23 14:56:41 -0300

    feat: styling

--- src/app/main/pages/admin/Approval/components/ExportButton.js
+++ src/app/main/pages/admin/Approval/components/ExportButton.js
@@ -161,7 +161,7 @@ function ExportButton({data}) {
                                 aria-controls="simple-menu"
                                 aria-haspopup="true"
                                 onClick={handleMenuClick}
-                                style={{ marginTop: '20px' }}
+                                // style={{ marginTop: '20px' }}
                             >
     
                                 <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium muiltr-hgpioi-MuiSvgIcon-root h-[2rem]" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SaveAltIcon"><path d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2z"></path></svg> Exportar
