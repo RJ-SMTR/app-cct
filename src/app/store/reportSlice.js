37d5f4aa8346b1b3a9ff44c15474712c0fa7df87
Gabriel Fortes <82231197+Gabriel0109@users.noreply.github.com>
2026-03-16 14:24:14 -0300

    Feat/pendencia paga (#474)
    * feat: filtros pendencia paga
    
    * finan-mov
    
    * Update reportSlice.js
    
    * Update FinancialMovement.js
    
    * Update reportSlice.js

--- src/app/store/reportSlice.js
+++ src/app/store/reportSlice.js
@@ -204,6 +204,26 @@ export const handleReportInfo = (data, reportType) => async (dispatch) => {
           };
           dispatch(handleSynthData(combinedResponse))
         } else {
+          if (
+            requestData.pendentes &&
+            responseData.data &&
+            Array.isArray(responseData.data) &&
+            reportType === 'consolidado' &&
+            requestData.consorcioNome?.length > 0
+          ) {
+            const mergedMap = responseData.data.reduce((acc, item) => {
+              const key = item.nomefavorecido;
+              if (acc[key]) {
+                acc[key].valor += item.valor;
+              } else {
+                acc[key] = { ...item };
+              }
+              return acc;
+            }, {});
+
+            responseData.data = Object.values(mergedMap);
+            responseData.valor = responseData.data.reduce((sum, item) => sum + item.valor, 0);
+          }
           dispatch(setReportList(responseData));
         }
         resolve(responseData);
