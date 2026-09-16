6d710d366d2ddbeb47d83387369741ed434df4a6
Gabriel Fortes <82231197+Gabriel0109@users.noreply.github.com>
2026-09-16 20:08:47 -0300

    feat: add table types

--- src/app/main/pages/admin/UserAnalysis/UserApp.js
+++ src/app/main/pages/admin/UserAnalysis/UserApp.js
@@ -13,7 +13,7 @@ import {
   useThemeMediaQuery
 } from '@fuse/hooks';
 import { Link, useParams } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { api } from 'app/configs/api/api';
 import Table from '../../extract/widgets/Table';
@@ -176,6 +176,11 @@ function UserApp() {
                 <div>
                   <Table id={id} userRoleId={user?.role?.id} />
                 </div>
+                {
+                  searchingDay ? <Box className='flex flex-col md:flex-row   justify-around'>
+                    <TableTypes />
+                  </Box> : <></>
+                }
                 {searchingWeek && !searchingDay ? <div>
                   <TablePending id={id} />
                 </div> : <></>}
